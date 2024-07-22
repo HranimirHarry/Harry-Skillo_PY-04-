87493bd3ea41481e151bb8db285f43082d425bad
HranimirHarry <pohari@gmail.com>
2024-07-22 19:35:40 +0200

    develop

---
+++ homework.py
@@ -0,0 +1,10 @@
+#
+print("Hello World!")
+
+name: str = "Hranimir."
+age: str = "44 years old."
+color: str = "Blue."
+print("My name is " +name)
+print("I am "+age)
+print("My favorite color is "+color)
+print("Nice to meet you.")
