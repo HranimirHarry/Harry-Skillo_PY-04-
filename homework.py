66059ecca57534f751175f2811b01867f9a41ff1
HranimirHarry <pohari@gmail.com>
2024-08-02 16:46:24 +0200

    develop

--- homework.py
+++ homework.py
@@ -9,6 +9,3 @@ print("""My name is Hranimir.
 I am 44 years old.
 My favorite color is Blue.""")
 
-
-#print("My name is", name, "I am", 44, "years old", "MY favorite color is", color, sep="\n")
-#print("My name is "+name, "I am "+age, "My favorite color is "+color, sep="\n")
